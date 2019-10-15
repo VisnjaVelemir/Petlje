5f5f7f9c36d392d464619247567a5c57abb52e97
Nikola <dzony.csplay@hotmail.com>
2019-10-15 21:52:30 +0200

    Grananje

---
+++ GrananjeIUslov/src/GrananjeIUslov.java
@@ -0,0 +1,61 @@
+
+public class GrananjeIUslov {
+
+	public static void main(String[] args) {
+
+int broj = 0;
+
+/*if (broj == 0){
+	System.out.println("EEE jeste broj pet!");	
+              }else {
+            	  System.out.println("NIJE broj 5!");
+              }
+		System.out.println("Idem dalje!");		
+if (broj < 0) {
+	System.out.println("Ovo je negativan broj");
+} else if (broj == 0) {
+	System.out.println( "Ovo je nula!!!");
+	}else {
+	System.out.println("Ovo je pozitivan broj");
+	
+
+	
+	
+	
+	
+}
+	
+
+*/ 
+
+
+
+/*int ocena = 5;
+if (ocena == 1) System.out.println("Nedovoljan 1");
+else if (ocena == 2) System.out.println("Dovoljan 2");
+else if (ocena == 3) System.out.println("Dobar 3");
+else if (ocena == 4) System.out.println("Vrlo dobar");
+else if (ocena == 5) System.out.println("Odlican 5");
+else System.out.println("Ocena je nepostojeca");*/
+
+
+
+String ime1 = "Nenad";
+String ime2 = "Pera";
+
+if (ime1 == ime2) System.out.println("Ista su!");
+else System.out.println("Nisu ista!");
+
+
+
+
+
+	}
+
+
+
+
+
+	
+	
+}
---
+++ GrananjeIUslov/src/GranjanjeDva.java
@@ -0,0 +1,85 @@
+
+public class GranjanjeDva {
+
+	public static void main(String[] args) {
+		
+		
+		int broj =7;
+		
+/*if (broj >=5 && broj <=11)	{	
+	System.out.println("Svidja mi se broj!");}
+
+else { System.out.println("Ne svidja mi se!");}
+
+
+if (broj <=5 || broj >=20) {
+	System.out.println("Svidjaju mi se brojevi");}
+
+
+if (broj >=5 && broj <=11 || broj <=20) 
+	System.out.println("Svidja mi se broj!");
+ else {System.out.println("Ne svidja mi se broj");}*/
+
+/*
+	
+		
+int number = 5;
+
+if(number == 5) {
+	System.out.println("Number je 5");
+} else {
+	System.out.println("Nije");
+}
+
+
+if(!(number == 5)) {
+System.out.println("Number je 5");	
+} else {
+	System.out.println("JESTE PET");
+}
+
+
+
+if (broj !=5) {
+	System.out.println("NIJE");
+} else {
+	System.out.println("JESTE 5");*/
+
+	
+	
+int danUnedelji = 5;
+
+switch (danUnedelji) {
+case 1: System.out.println ("PONEDELJAK");
+break;
+
+case 2: System.out.println("UTORAK");
+break;
+
+case 3: System.out.println("SREDA");
+break;
+
+case 4: System.out.println("CETVRTAK");
+break;
+
+case 5: System.out.println("PETAK");
+break;
+
+case 6: System.out.println("SUBOTA");
+break;
+
+case 7: System.out.println("NEDELJA");
+break;
+
+default: System.out.println("Ne postoji taj dan u nedelji");
+	break;
+
+	}
+	}
+}
+
+
+
+
+
+
